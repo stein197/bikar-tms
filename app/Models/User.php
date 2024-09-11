4260aa41b3b748a9f53c5730118f7facfc6b90a1
Nail Gafarov <nil20122013@gmail.com>
2024-09-11 12:33:11 +0200

    Added model for tasks, added relationships and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -31,6 +32,10 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function tasks(): HasMany {
+        return $this->hasMany(Task::class);
+    }
 
     /**
      * Get the attributes that should be cast.
